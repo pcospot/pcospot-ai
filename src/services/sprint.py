00495b48e57a47c5070be13fa0033ef688972633
Juan Lee <ninejuan@protonmail.com>
2024-11-11 22:01:42 +0900

    add: sprint support system prompt

--- src/services/sprint.py
+++ src/services/sprint.py
@@ -10,13 +10,28 @@ client = OpenAI(
 )
 
 # 요일, 주제(할 일: ex 게시판 생성 등)를 전달받아 생성
-def generateSprint(startDate: str, endDate: str, subject: str, numOfPeople: str):
+def generateSprint(startDate: str, endDate: str, subject: str, numOfPeople: str) -> str:
     try:
         completion = client.chat.completions.create(
             model=f"{model_id}",
             messages=[
                 {"role": "system", "content": "너는 스프린트 일정을 만드는 AI야. 너에게는 시작 요일, 종료 요일, 스프린트 주제, 인원 수가 주어져."},
                 {"role": "user", "content": f"시작 요일은 {startDate}이고, 종료 요일은 {endDate}, 참여 인원은 {numOfPeople}이야. 스프린트 주제는 {subject}야. 요일별 스프린트를 작성해줘."}
+            ],
+            temperature=0.6
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        raise e
+    
+# 스프린트 감독 AI : 지금까지 스프린트 잘 하고 있는지 : 정확히는 조언하는 기능
+def sprintSupport(elapsedDate: int) -> str:
+    try:
+        completion = client.chat.completions.create(
+            model=f"{model_id}",
+            messages=[
+                {"role": "system", "content": "너는 한 조직의 스프린트 감독관이야. 주어진 스프린트 상황에 맞는 피드백을 단 한 줄로 해야 해. 잊지 마. 단 한 줄로 작성해야 해."},
+                {"role": "user", "content": f""}
             ],
             temperature=0.6
         )
