5dfaada74eba12224f264ca36df4fcbab3b64c7f
Juan Lee <ninejuan@protonmail.com>
2024-11-12 00:18:54 +0900

    add: sprint user prompt

--- src/services/sprint.py
+++ src/services/sprint.py
@@ -25,16 +25,18 @@ def generateSprint(startDate: str, endDate: str, subject: str, numOfPeople: str)
         raise e
     
 # 스프린트 감독 AI : 지금까지 스프린트 잘 하고 있는지 : 정확히는 조언하는 기능
-def sprintSupport(elapsedDate: int) -> str:
+# parameter로 스프린트 시작 후 경과일, 스프린트 관련 정보를 입력받습니다.
+def sprintSupport(elapsedDate: int, lastDate: int, sprintData: str) -> str:
     try:
         completion = client.chat.completions.create(
             model=f"{model_id}",
             messages=[
                 {"role": "system", "content": "너는 한 조직의 스프린트 감독관이야. 주어진 스프린트 상황에 맞는 피드백을 단 한 줄로 해야 해. 잊지 마. 단 한 줄로 작성해야 해."},
-                {"role": "user", "content": f""}
+                {"role": "user", "content": f"스프린트 시작 후 경과일: {elapsedDate}일, 잔여 기간: {lastDate}일, 스프린트 정보: {sprintData}. "}
             ],
             temperature=0.6
         )
+        print(completion.choices[0].message.content)
         return completion.choices[0].message.content
     except Exception as e:
         raise e
