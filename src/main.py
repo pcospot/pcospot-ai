dccb959c68c037a1c4b7c36b4db8bb29626ee09b
Juan Lee <ninejuan@protonmail.com>
2024-11-12 21:35:11 +0900

    remove: temporal code

--- src/main.py
+++ src/main.py
@@ -28,6 +28,4 @@ app.include_router(sprint.router)
 app.include_router(summarize.router)
 
 if __name__ == "__main__":
-    import services.summarizeChat as sc
-    sc.summarize("""""")
     uvicorn.run(app, host="0.0.0.0", port=9000)
